fc42d7c427a3a1b35add63c3a0e420a2f4b3cf10
ksammar <karam.sammar@intel.com>
2025-06-08 17:40:56 +0300

    added parser for plist

--- flow_indicator/include/FlowIndicator.h
+++ flow_indicator/include/FlowIndicator.h
@@ -1,6 +1,8 @@
 #ifndef FLOW_INDICATOR_H_
 #define FLOW_INDICATOR_H_
 
+#include "Helper.h"
+#include <vector>
 class ProgramManager
 {
 public:
@@ -9,15 +11,17 @@ public:
     ProgramManager& operator=(const ProgramManager& rhs) = delete;
 
     void Start(const int argc, const char** argv);
-    void PrintHowToOperate();
+  //  void PrintHowToOperate();
 };
 
 
 class FlowIndicatorPlistPreCheck
 {
 public:
-    void RunflwIndPlistStage(const int argc,const std::string& argv);
-
+    void RunflwIndPlistStage(const int& argc,const std::string& fileName);
+    void PListParser(const std::string& pListPath);
+private:
+    std::vector<std::string> m_patNames;
 
 };
 
--- flow_indicator/src/FlowIndicator.cpp
+++ flow_indicator/src/FlowIndicator.cpp
@@ -14,7 +14,7 @@ void ProgramManager::Start(const int m_argc, const char** m_argv)
     if (2 == m_argc)
     {
         FlowIndicatorPlistPreCheck stage1;
-        std::string argv1 = m_argv[1];
+        const std::string argv1 = m_argv[1];
         if (std::string::npos != argv1.find("-PLIST:"))
         {
             stage1.RunflwIndPlistStage(m_argc, argv1);
@@ -22,13 +22,48 @@ void ProgramManager::Start(const int m_argc, const char** m_argv)
     }
 }
 
-void FlowIndicatorPlistPreCheck::RunflwIndPlistStage(const int& argc,const std::string& argv)
+void FlowIndicatorPlistPreCheck::RunflwIndPlistStage(const int& argc, const std::string& fileName)
 {
 
+    std::string pListPath;
+    const std::string askForFilemsg = "Enter name of Plist file like this: -PLIST:plist_example.plist";
+    //change name it not only for files //put it in string helper
+    pListPath = ConsoleHelper::GetFileNameFromCommandLine(std::string("-PLIST:"), askForFilemsg, fileName);
+  //  PListParser(pListPath);
+}
 
-const std::string askForFlowFilemsg = "Enter name of flow file like this: -FLOW:flow_example.flw";
-//change name it not only for files //put it in string helper
-flowFile = ConsoleHelper::GetFileNameFromCommandLine("-FLOW:", askForFlowFilemsg, argv[1]);
+void FlowIndicatorPlistPreCheck::PListParser(const std::string& pListPath)
+{
+    std::ifstream file(pListPath); // Replace with your actual file name
+    std::string line;
 
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << pListPath << std::endl;
+        return;
+    }
+    while (std::getline(file, line)) {
+        size_t pos = line.find("Pat");
+        if (pos != std::string::npos) {
+            // Find the start of the name after "Pat"
+            size_t start = line.find_first_not_of(" \t", pos + 3);
+            if (start != std::string::npos) {
+                // Find the end of the name using space as delimiter
+                size_t end = line.find_first_of(" \t;", start);
+                std::string name = line.substr(start, end - start);
 
-]
+                // Remove semicolon at the end if present
+                if (!name.empty() && name.back() == ';') {
+                    name.pop_back();
+                }
+
+                m_patNames.push_back(name);
+            }
+        }
+    }
+    file.close();
+    // Output the names
+    std::cout << "Extracted Pat names:" << std::endl;
+    for (const auto& name : m_patNames) {
+        std::cout << name << std::endl;
+    }
+}
